36a91929ff473584c0acf8be087e46a0e439fbc9
maxime.proulx <dro@Maximes-MacBook-Pro-2.local>
2015-05-26 20:11:44 -0400

    Aded recipes to the repo

--- Source/CookItNow.Api/Models/QuickRecipe.cs
+++ Source/CookItNow.Api/Models/QuickRecipe.cs
@@ -10,20 +10,31 @@ namespace CookItNow.Api.Models
 		public string Time { get; set; }
 	}
 	
+	public class SubRecipe
+	{
+		public int Id { get; set; }
+		
+		public string Title { get; set; }
+	}
+	
 	public class Ingredient 
 	{
 		public long Id { get; set; }
+		
+		public int? SubRecipeId { get; set; }
 		
 		public string Name { get; set; }
 		
 		public Quantity Quantity { get; set; }
 		
 		public Ingredient Replacement { get; set; }
+		
+		public IEnumerable<string> Requirements { get; set; }
 	}
 	
 	public class Quantity 
 	{
-		public int Value { get; set; }
+		public double Value { get; set; }
 		
 		public MeasureUnit OriginalMeasureUnit { get; set; }
 	}
@@ -36,12 +47,18 @@ namespace CookItNow.Api.Models
 
         Litre, 
 
-        Units
+        Units, 
+		
+		Pinch, 
+		
+		Cup
 	}
 	
 	public class RecipeStep  
 	{
 	    public string Description { get; set; }
+		
+		public int? SubRecipeId { get; set; }
 
 	    public RecipeStep PostStep { get; set; }
 	}
@@ -60,60 +77,10 @@ namespace CookItNow.Api.Models
 		
 		public IEnumerable<Duration> Durations { get; set; }
 		
+		public IEnumerable<SubRecipe> SubRecipes { get; set; }
+		
 		public IEnumerable<Ingredient> Ingredients { get; set; }
 		
 		public IEnumerable<RecipeStep> Steps { get; set; }
-		
-// 		{
-//     "Id": 2,
-//     "Title": "Gaufres",
-//     "OriginalUrl": "http://www.ricardocuisine.com/recettes/3892-gaufres",
-//     "Summary": "",
-//     "OriginalServings": 8,
-//     "Durations": [
-//         {
-//             "Title": "Préparation",
-//             "Time": "PT0H10M"
-//         }
-//     ],
-//     "Ingredients": [
-//         {
-//             "Id": 1,
-//             "Name": "Lait de beurre",
-//             "Quantity": {
-//                 "Value": 250,
-//                 "OriginalMeasureUnit": "ml"
-//             },
-// 			"Replacements": [
-//                 {
-// 					"Name": "Lait",
-// 					"Quantity": {
-//                         "Value": 250,
-//                         "OriginalMeasureUnit": "ml"
-// 				      }
-//                 }
-//             ]
-//         }
-//     ],
-//     "Steps": [
-//         {
-//             "Description": "Dans un bol, {action:'Mélanger'} {ingredientId:2}, {ingredientId:3}, {ingredientId:6}, {ingredientId:7} et {ingredientId:8}. {action:'Réserver'}."
-//         },
-//         {
-//             "Description": "Dans un autre bol, {action:'Fouetter'} {ingredientId:9} et {ingredientId:5} environ {timer:'PT0H05M'} jusqu'à ce qu'ils blanchissent, forment un ruban et triplent de volume.",
-//             "PostStep": 
-//             {
-//                 "Description": "{action:'Ajouter'} {ingredientId:4} graduellement en fouettant. À l'aide dune spatule, {action:'Incorporer'} les ingrédients secs en alternant avec {ingredientId:1} jusqu'à ce que le mélange soit humecté (il restera quelques grumeaux de farine). {action:'Laisser reposer'} {timer:'PT0H10M'}."
-//             }
-//         },
-//         {
-//             "Description": "{action:'Préchauffer'} le gaufrier. {action:'Verser'} environ 250 ml (1 tasse) de pâte à la fois (pour deux gaufres) et {action:'Refermer'} l'appareil (ou selon les recommandations du fabricant). {action:'Cuire'} environ {timer:'PT0H05M'} ou jusqu'à ce qu'elles soient dorées.",
-//             "PostStep":
-//             {
-//                 "Description": "{action:'Réserver'} au chaud. {action:'Répéter'} l'opération avec le reste de la pâte. {action:'Servir'} avec le coulis de fraises en trempette."
-//             } 
-//         }
-//     ]
-// }
 	}
 }
