32a7ebe25d24dc1abfbd6561da1601b4a1bfd4cd
maxime.proulx <dro@Maximes-MacBook-Pro-2.local>
2015-06-10 17:09:45 -0400

    arranged sentnces, capital letters and periods. Added glyphicon for timer

--- Source/CookItNow.Web/Scripts/app/ValueConverters/step-item.ts
+++ Source/CookItNow.Web/Scripts/app/ValueConverters/step-item.ts
@@ -2,13 +2,13 @@ import {DurationFormatValueConverter} from "ValueConverters/duration-format";
 import {SanitizeHtmlValueConverter} from "aurelia-templating-resources/sanitize-html";
 
 export class StepItemValueConverter {
-	toView(value, ingredients: {}[]) {
+	toView(value: string, ingredients: {}[]) {
 		var match;
 		while (match = /{action:'\D+'}/.exec(value)) {
 			var action = match[0].replace("{action:'", "").replace("'}", "");
-			var actionHtml = "<span class='action'>" + action.toLowerCase() + "</span>";
+			var actionHtml = "<span class='action'>" + action + "</span>";
 			value = value.replace(match[0], actionHtml);
-		};
+		}
 		
 		while (match = /{ingredient:\d+}/.exec(value)) {
 			var ingredientId = match[0].replace("{ingredient:", "").replace("}", "");
@@ -17,28 +17,51 @@ export class StepItemValueConverter {
 				(item) => item["id"] == ingredientId
 			)[0];
 			
+			var ingredientName = referencedIngredient["name"].toLowerCase();
+			var ingredientNameFirstLetter = ingredientName[0];
+			var nextWord = 
+				ingredientNameFirstLetter === "a" 
+				|| ingredientNameFirstLetter === "e"
+				|| ingredientNameFirstLetter === "i"
+				|| ingredientNameFirstLetter === "o"
+				|| ingredientNameFirstLetter === "u"
+				|| ingredientNameFirstLetter === "y"
+				|| ingredientNameFirstLetter === "h"
+				? "d' "
+				: "de ";
+			
 			var ingredientHtml = 
 				"<span class='ingredient'>" + 
-					"<span class='value'>" + referencedIngredient["quantity"]["value"] + "</span>" + 
+					"<span class='value'>" + referencedIngredient["quantity"]["value"].toString().replace(".", ",") + "</span>" + 
 					"<span class='type'>" + referencedIngredient["quantity"]["originalMeasureUnit"] + "</span>" +
-					" de " + 
-					"<span class='value'>" + referencedIngredient["name"].toLowerCase() + "</span>" +
+					" " + nextWord + "" + 
+					"<span class='value'>" + ingredientName + "</span>" +
 				"</span>";
 			
 			value = value.replace(match[0], ingredientHtml);
-		};
+		}
 		
 		while (match = /{timer:'PT\d\dH\d\dM'}/.exec(value)) {
 			var formattedDuration = new DurationFormatValueConverter().toView(match[0]);
-			var durationHtml = "<span class='timer'>" + formattedDuration + "</span>";
+			
+			var durationHtml = 
+				"<span class='timer'>" +
+					"<a class='glyphicon glyphicon-time'>" + 
+						formattedDuration + 
+					"</a>" + 
+				"</span>";
 			
 			value = value.replace(match[0], durationHtml);
-		};
+		}
 		
-		while (match = /\./.exec(value)) {
-			var lineBreak = match[0].replace(".", "<br />");
-			value = value.replace(match[0], lineBreak);
-		};
+		var splits = value.split(".");
+		splits.forEach(function(split) {
+			if (split !== "") {
+				var newSplit = split.trim();
+				newSplit += ".<br />";
+				value = value.replace(split + ".", newSplit);
+			}
+		}, this);
 		
 		var sanitizedValue = new SanitizeHtmlValueConverter().toView(value);
 		
--- Source/CookItNow.Web/Scripts/app/recipes.ts
+++ Source/CookItNow.Web/Scripts/app/recipes.ts
@@ -168,19 +168,19 @@ export class Pouding extends QuickRecipe {
             "steps": [
                 {
                     "subrecipeId": 1, 
-                    "description": "Dans une casserole, {action:'Mélanger'} {ingredient:1} et {ingredient:3}. {action:'Ajouter'} {ingredient:5}. {action:'Ajouter'} {ingredient:2} et {ingredient:4}. {action:'Porter à ébullition'} en remuant à l'aide d'un fouet. {action:'Incorporer'} {ingredient:6}. {action:'Réserver'}"
+                    "description": "Dans une casserole, {action:'mélanger'} {ingredient:1} et {ingredient:3}. {action:'Ajouter'} {ingredient:5}. {action:'Ajouter'} {ingredient:2} et {ingredient:4}. {action:'Porter à ébullition'} en remuant à l'aide d'un fouet. {action:'Incorporer'} {ingredient:6}. {action:'Réserver'}."
                 },
                 {
                     "subrecipeId": 2, 
-                    "description": "Dans une petite casserole, {action:'Porter à ébullition'} {ingredient:9} et {ingredient:10} en remuant à l’aide d’un fouet. {action:'Laisser tiédir'}. {action:'Gosser'} {timer:'PT00H02M'}." 
+                    "description": "Dans une petite casserole, {action:'porter à ébullition'} {ingredient:9} et {ingredient:10} en remuant à l’aide d’un fouet. {action:'Laisser tiédir'}. {action:'Gosser'} {timer:'PT00H02M'}." 
                 },
                 {
                     "subrecipeId": 2,
-                    "description": "Dans un bol, {action:'Mélanger'} {ingredient:7}, {ingredient:12} et {ingredient:13}. {action:'Réserver'}." 
+                    "description": "Dans un bol, {action:'mélanger'} {ingredient:7}, {ingredient:12} et {ingredient:13}. {action:'Réserver'}." 
                 },
                 {
                     "subrecipeId": 2,
-                    "description": "Dans un autre bol, {action:'Mélanger'} {ingredient:11} et {ingredient:8} au batteur électrique jusqu’à ce que le mélange prenne une texture granuleuse. {action:'Ajouter'} les oeufs, un à la fois, et {action:'Battre'} jusqu’à ce que la préparation soit homogène. À basse vitesse, {action:'Incorporer'} les ingrédients secs en alternant avec le mélange de cacao. {action:'Répartir'} la pâte dans le plat." 
+                    "description": "Dans un autre bol, {action:'mélanger'} {ingredient:11} et {ingredient:8} au batteur électrique jusqu’à ce que le mélange prenne une texture granuleuse. {action:'Ajouter'} les oeufs, un à la fois, et {action:'battre'} jusqu’à ce que la préparation soit homogène. À basse vitesse, {action:'incorporer'} les ingrédients secs en alternant avec le mélange de cacao. {action:'Répartir'} la pâte dans le plat." 
                 },
                 {
                     "subrecipeId": 2,
@@ -297,17 +297,17 @@ export class Gaufres extends QuickRecipe {
             ],
             "steps": [
                 {
-                    "description": "Dans un bol, {action:'Mélanger'} {ingredient:2}, {ingredient:3}, {ingredient:6}, {ingredient:7} et {ingredient:8}. {action:'Réserver'}."
+                    "description": "Dans un bol, {action:'mélanger'} {ingredient:2}, {ingredient:3}, {ingredient:6}, {ingredient:7} et {ingredient:8}. {action:'Réserver'}."
                 },
                 {
-                    "description": "Dans un autre bol, {action:'Fouetter'} {ingredient:9} et {ingredient:5} environ {timer:'PT0H05M'} jusqu'à ce qu'ils blanchissent, forment un ruban et triplent de volume.",
+                    "description": "Dans un autre bol, {action:'fouetter'} {ingredient:9} et {ingredient:5} environ {timer:'PT0H05M'} jusqu'à ce qu'ils blanchissent, forment un ruban et triplent de volume.",
                     "postStep": 
                     {
-                        "description": "{action:'Ajouter'} {ingredient:4} graduellement en fouettant. À l'aide dune spatule, {action:'Incorporer'} les ingrédients secs en alternant avec {ingredient:1} jusqu'à ce que le mélange soit humecté (il restera quelques grumeaux de farine). {action:'Laisser reposer'} {timer:'PT00H10M'}."
+                        "description": "{action:'Ajouter'} {ingredient:4} graduellement en fouettant. À l'aide dune spatule, {action:'incorporer'} les ingrédients secs en alternant avec {ingredient:1} jusqu'à ce que le mélange soit humecté (il restera quelques grumeaux de farine). {action:'Laisser reposer'} {timer:'PT00H10M'}."
                     }
                 },
                 {
-                    "description": "{action:'Préchauffer'} le gaufrier. {action:'Verser'} environ 250 ml (1 tasse) de pâte à la fois (pour deux gaufres) et {action:'Refermer'} l'appareil (ou selon les recommandations du fabricant). {action:'Cuire'} environ {timer:'PT00H05M'} ou jusqu'à ce qu'elles soient dorées.",
+                    "description": "{action:'Préchauffer'} le gaufrier. {action:'Verser'} environ 250 ml (1 tasse) de pâte à la fois (pour deux gaufres) et {action:'refermer'} l'appareil (ou selon les recommandations du fabricant). {action:'Cuire'} environ {timer:'PT00H05M'} ou jusqu'à ce qu'elles soient dorées.",
                     "postStep":
                     {
                         "description": "{action:'Réserver'} au chaud. {action:'Répéter'} l'opération avec le reste de la pâte. {action:'Servir'} avec le coulis de fraises en trempette."
