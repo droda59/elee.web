5c7d4e80eb4eb3f22fea86f97c76470fc0e3e59b
Maxime Gaudreault Proulx <swcommander@gmail.com>
2015-06-28 15:21:02 -0400

    fixed materialize dependency

--- Source/CookItNow.Web/Scripts/app/welcome.ts
+++ Source/CookItNow.Web/Scripts/app/welcome.ts
@@ -1,6 +1,14 @@
 import {jQuery} from "jquery";
 
 export class Welcome {
+	selectedRecipeId: number;
+	recipes: {}[] = [];
+	
+	constructor() {
+		this.recipes.push({ id: 1, title: "Pouding au chocolat" });
+		this.recipes.push({ id: 2, title: "Gaufres" });
+	}
+	
 	attached() {
 	    $('.parallax').parallax();
 	}
