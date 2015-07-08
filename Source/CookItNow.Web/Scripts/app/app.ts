90d9ed52ac3c437adb55448a2692f5682848fd4f
Maxime Gaudreault Proulx <swcommander@gmail.com>
2015-07-08 00:12:27 -0400

    Removed unused views

--- Source/CookItNow.Web/Scripts/app/app.ts
+++ Source/CookItNow.Web/Scripts/app/app.ts
@@ -7,8 +7,6 @@ export class App {
     config.title = "Cook it now";
     config.map([
       { route: ["","welcome"],  moduleId: "welcome",      nav: true, title:"Welcome" },
-      { route: "form",  moduleId: "form",      nav: true, title:"Form" },
-      { route: "child-router",  moduleId: "child-router", nav: true, title:"Child Router" },
       { route: "recipe/:id", name: "quick-recipe", moduleId: "quick-recipe-page" }, 
     ]);
 
