4daed69ff02be663c06d0ea9a2fbdd3285c35571
Maxime Gaudreault Proulx <swcommander@gmail.com>
2015-08-13 16:22:01 -0400

    UI: Changed step-ingredient to use convertible quantities

--- Source/CookItNow.Web/app/quick-recipe/components/step-ingredient.ts
+++ Source/CookItNow.Web/app/quick-recipe/components/step-ingredient.ts
@@ -1,26 +1,10 @@
 import {IngredientPart} from "quick-recipe/models/quick-recipe";
-import {Quantity} from "shared/models/quantity";
 import {Ingredient} from "shared/models/ingredient";
-import {TextUtils} from "shared/text-utils";
 
 export class StepIngredient {
-	measureUnit: string;
-	quantity: Quantity;
-	nextWord: string;
-	ingredientName: string;
-	requirements: string;
-	
-	private _ingredient: Ingredient;
+	ingredient: Ingredient;
 	
 	activate(model: IngredientPart) {
-    	this._ingredient = model.ingredient;
-		
-		this.ingredientName = this._ingredient.name.toLowerCase();
-		this.nextWord = (this._ingredient.quantity.originalMeasureUnit !== "unit" ? TextUtils.isVowel(this.ingredientName[0]) ? " d'" : " de " : " ");
-			
-		this.measureUnit = this._ingredient.quantity.originalMeasureUnit;
-		this.quantity = new Quantity(this._ingredient.quantity);
-		
-		this.requirements = (this._ingredient.requirements || []).join(" et ");
+    	this.ingredient = model.ingredient;
 	}
 }
