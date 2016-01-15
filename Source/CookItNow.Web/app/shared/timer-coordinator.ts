2625e405a959f1177729fa8b7c4b08bf1c27eeb8
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-01-14 22:59:12 -0500

    Removed weird file

--- Source/CookItNow.Web/app/shared/timer-coordinator.ts
+++ Source/CookItNow.Web/app/shared/timer-coordinator.ts
@@ -1,7 +1,6 @@
 import {inject} from "aurelia-framework";
 import {I18N} from "aurelia-i18n";
 import {Timer} from "shared/models/timer";
-import {Notification} from "chrome.notifications";
 
 @inject (I18N)
 export class TimerCoordinator {
