e88e55aa8bed61c875c1443984a76d4ade2da1c9
Marc-André Roy <marc596@gmail.com>
2016-07-15 16:05:43 -0400

    Add a new footer component for the website part.

---
+++ Source/app/shared/components/page-footer/page-footer.ts
@@ -0,0 +1,16 @@
+import {autoinject} from "aurelia-framework";
+import {I18N, BaseI18N} from "aurelia-i18n";
+import {EventAggregator} from "aurelia-event-aggregator";
+
+@autoinject
+export class PageFooter extends BaseI18N {
+    private _i18n: I18N;
+    private _element: Element;
+
+    constructor(i18n: I18N, element: Element, ea: EventAggregator) {
+        super(i18n, element, ea);
+
+        this._i18n = i18n;
+        this._element = element;
+    }
+}
