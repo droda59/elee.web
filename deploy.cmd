a8045c878a3f21b43afa55514908e635c63b4813
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-08-02 23:29:19 -0400

    Subdirectory copying

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Deploying files...
-xcopy "$DEPLOYMENT_SOURCE/Source/export" "%DEPLOYMENT_TARGET%" /Y
+xcopy "$DEPLOYMENT_SOURCE/Source/export" "%DEPLOYMENT_TARGET%" /Y /S
 
 echo "Finished successfully."
