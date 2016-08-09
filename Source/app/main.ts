4a85930e16b1aa2c463190ad83c70147d57b1f55
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-08-08 21:13:12 -0400

    ELEE-32: Fixed browser title localization

--- Source/app/main.ts
+++ Source/app/main.ts
@@ -1,13 +1,21 @@
+import {inject} from "aurelia-framework";
 import {Router, RouterConfiguration} from "aurelia-router";
+import {I18N} from "aurelia-i18n";
 
+@inject (I18N)
 export class Main {
 	router: Router;
+    private _i18n: I18N;
+
+    constructor(i18n: I18N) {
+        this._i18n = i18n;
+    }
 
 	configureRouter(config: RouterConfiguration, router: Router) {
 		config.title = "E133";
 		config.map([
-			{ route: ["", "home"], name: "home", moduleId: "app/website/home", nav: true, title: "Website Home" },
-			{ route: "typeform", name: "typeform", moduleId: "app/shared/components/typeform", nav: true, title: "Comments" },
+			{ route: ["", "home"], name: "home", moduleId: "app/website/home", nav: true },
+			{ route: "typeform", name: "typeform", moduleId: "app/shared/components/typeform", nav: true, title: this._i18n.tr("comments.pageTitle") },
 			{ route: "recipe/:id", name: "quick-recipe", moduleId: "app/quick-recipe/quick-recipe-page" }
 		]);
 
--- Source/app/website/home.ts
+++ Source/app/website/home.ts
@@ -1,13 +1,20 @@
+import {inject} from "aurelia-framework";
 import {Router, RouterConfiguration} from "aurelia-router";
+import {I18N} from "aurelia-i18n";
 
+@inject (I18N)
 export class Home {
     private _router: Router;
+    private _i18n: I18N;
+
+    constructor(i18n: I18N) {
+        this._i18n = i18n;
+    }
 
     configureRouter(config: RouterConfiguration, router: Router) {
-        config.title = "E133";
         config.map([
-            { route: ["", "welcome"], name: "welcome", moduleId: "app/website/welcome/welcome", nav: true, title: "Welcome" },
-            { route: "about", name: "about", moduleId: "app/website/about/about" }
+            { route: ["", "welcome"], name: "welcome", moduleId: "app/website/welcome/welcome", nav: true, title: this._i18n.tr("home.pageTitle") },
+            { route: "about", name: "about", moduleId: "app/website/about/about", nav: true, title: this._i18n.tr("about.pageTitle") }
         ]);
 
         this._router = router;
