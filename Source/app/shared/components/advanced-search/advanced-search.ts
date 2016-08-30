4a198a0b9c82312a6ec4783ea1812a03549fb7df
Marc-André Roy <marc596@gmail.com>
2016-08-29 22:51:20 -0400

    Basic AdvancedSearch control. No styling at all, binding working. Really the beginning of everything.

--- Source/app/shared/components/advanced-search/advanced-search.ts
+++ Source/app/shared/components/advanced-search/advanced-search.ts
@@ -4,7 +4,7 @@ import {I18N, BaseI18N} from "aurelia-i18n";
 
 @inject (I18N, Element, EventAggregator)
 export class AdvancedSearch extends BaseI18N {
-    @bindable recipeTime: string = null;
+    @bindable time: string = null;
     @bindable ingredients: string[] = [];
 
     private _i18n: I18N;
