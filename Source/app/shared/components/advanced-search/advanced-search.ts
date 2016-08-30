2c8875016c1ae42798e25ab19db281571b4621a0
Marc-André Roy <marc596@gmail.com>
2016-08-29 21:52:21 -0400

    Base plumbing for AdvancedSearch component

---
+++ Source/app/shared/components/advanced-search/advanced-search.ts
@@ -0,0 +1,19 @@
+import {inject, bindable} from "aurelia-framework";
+import {EventAggregator} from "aurelia-event-aggregator";
+import {I18N, BaseI18N} from "aurelia-i18n";
+
+@inject (I18N, Element, EventAggregator)
+export class AdvancedSearch extends BaseI18N {
+    @bindable recipeTime: string = null;
+    @bindable ingredients: string[] = [];
+
+    private _i18n: I18N;
+    private _element: Element;
+
+    constructor(i18n: I18N, element: Element, ea: EventAggregator) {
+        super(i18n, element, ea);
+
+        this._i18n = i18n;
+        this._element = element;
+    }
+}
