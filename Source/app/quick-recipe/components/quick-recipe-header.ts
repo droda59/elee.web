9263041cab0eafcf4be9d0ee612b51f963ae49ab
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-09-04 15:42:57 -0400

    Added a Report button

--- Source/app/quick-recipe/components/quick-recipe-header.ts
+++ Source/app/quick-recipe/components/quick-recipe-header.ts
@@ -1,25 +1,45 @@
 import {bindable, inject} from "aurelia-framework";
 import {DialogService} from "aurelia-dialog";
+import {HttpClient} from "aurelia-http-client";
+import {I18N} from "aurelia-i18n";
 import {SettingsManager} from "app/shared/settings-manager";
 import {SettingsModal} from "app/shared/components/settings-modal";
 
-@inject(DialogService, SettingsManager)
+@inject (HttpClient, DialogService, SettingsManager, I18N)
 export class QuickRecipeHeader {
-  private _dialogService: DialogService;
-  private _settingsManager: SettingsManager;
+	@bindable recipeId: string = null;
 
-  constructor(dialogService: DialogService, settingsManager: SettingsManager) {
-    this._dialogService = dialogService;
-    this._settingsManager = settingsManager;
-  }
+	private _dialogService: DialogService;
+	private _settingsManager: SettingsManager;
+    private _http: HttpClient;
+	private _i18n: I18N;
 
-  editSettings(): void {
-    this._dialogService
-      .open({ viewModel: SettingsModal, model: this._settingsManager.settings })
-      .then((result) => {
-        if (!result.wasCancelled) {
-          this._settingsManager.save(result.output);
-        }
-      });
-  }
+	constructor(http: HttpClient, dialogService: DialogService, settingsManager: SettingsManager, i18n: I18N) {
+		this._http = http;
+		this._i18n = i18n;
+		this._dialogService = dialogService;
+		this._settingsManager = settingsManager;
+	}
+
+	attached(): void {
+	    $(".tooltipped").tooltip({ delay: 50 });
+	}
+
+	editSettings(): void {
+		this._dialogService
+			.open({ viewModel: SettingsModal, model: this._settingsManager.settings})
+			.then((result) => {
+				if (!result.wasCancelled) {
+					this._settingsManager.save(result.output);
+				}
+			});
+	}
+
+	reportRecipe(): void {
+		this._http
+            .put("http://eleeapi.azurewebsites.net/api/review/flag/" + this.recipeId)
+            .then(response => {
+                Materialize.toast(this._i18n.tr("quickRecipe.recipeFlagged"), 3000)
+            });
+	}
 }
