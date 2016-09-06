cf200e0b4858852ae9648e89eb35c7fb4b8badaa
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-09-05 21:39:35 -0400

    ELEE-63: Added steps types

---
+++ Source/app/quick-recipe/components/edition-steps/step-text.ts
@@ -0,0 +1,9 @@
+import {TextPart} from "app/quick-recipe/models/quick-recipe";
+
+export class StepText {
+    value: string;
+
+    activate(model: TextPart) {
+        this.value = model.value;
+    }
+}
---
+++ Source/app/quick-recipe/components/edition-steps/step-timer.ts
@@ -0,0 +1,19 @@
+import {inject} from "aurelia-framework";
+import {QuickRecipeTimer} from "app/quick-recipe/models/quick-recipe-timer";
+import {TimerPart} from "app/quick-recipe/models/quick-recipe";
+import {TimerCoordinator} from "app/quick-recipe/timer-coordinator";
+
+@inject(TimerCoordinator)
+export class StepTimer {
+    timer: QuickRecipeTimer;
+
+    private _timerCoordinator: TimerCoordinator;
+
+    constructor(timerCoordinator: TimerCoordinator) {
+        this._timerCoordinator = timerCoordinator;
+    }
+
+    activate(model: TimerPart) {
+        this.timer = new QuickRecipeTimer(model.stepId, model.value, model.action, model.text);
+    }
+}
