db8e3ef3a610cd76a94e2d1850c193a36c6f5390
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-09-06 13:42:41 -0400

    ELEE-63: Added enumeration part binding

--- Source/app/quick-recipe/components/edition-steps/step-enumeration.ts
+++ Source/app/quick-recipe/components/edition-steps/step-enumeration.ts
@@ -1,10 +1,33 @@
 import {IngredientEnumerationPart} from "app/quick-recipe/models/quick-recipe";
 import {Ingredient} from "app/shared/models/ingredient";
+import {EditRecipePage} from "app/quick-recipe/edit-recipe-page";
 
+@autoinject()
 export class StepEnumeration {
     ingredients: Ingredient[];
+    possibleIngredients: Ingredient[] = [];
+    parentelement: EditRecipePage;
+
+    constructor(parentelement: EditRecipePage){
+        this.parentelement = parentelement;
+    }
 
     activate(model: IngredientEnumerationPart) {
         this.ingredients = model.ingredients;
+
+        var usedSubrecipes = this.ingredients.map(ingredient => ingredient.subrecipeId);
+        var subrecipe = this.parentelement.subrecipes.filter(subrecipe => usedSubrecipes.contains(subrecipe.id))[0];
+        this.possibleIngredients = subrecipe.ingredients;
+    }
+
+    removeIngredient(ingredient: Ingredient): void {
+        this._removeFromArray(this.ingredients, ingredient);
+    }
+
+    private _removeFromArray(array: any[], object: any): void {
+        var index = array.indexOf(object);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
     }
 }
