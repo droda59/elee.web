d3ac43cf53311ad584a4fb20f763a157a52389a5
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-09-11 23:47:18 -0400

    Created a new quick recipe proxy service

--- Source/app/quick-recipe/components/quick-recipe-header.ts
+++ Source/app/quick-recipe/components/quick-recipe-header.ts
@@ -1,23 +1,23 @@
-import {bindable, inject} from "aurelia-framework";
+import {bindable, autoinject} from "aurelia-framework";
 import {Router} from "aurelia-router";
 import {DialogService} from "aurelia-dialog";
-import {HttpClient} from "aurelia-http-client";
 import {I18N} from "aurelia-i18n";
+import {QuickRecipeService} from "app/shared/quick-recipe-service";
 import {SettingsManager} from "app/shared/settings-manager";
 import {SettingsModal} from "app/shared/components/settings-modal";
 
-@inject (HttpClient, DialogService, SettingsManager, I18N, Router)
+@autoinject()
 export class QuickRecipeHeader {
 	@bindable recipeId: string = null;
 
 	private _router: Router;
 	private _dialogService: DialogService;
 	private _settingsManager: SettingsManager;
-    private _http: HttpClient;
+    private _service: QuickRecipeService;
 	private _i18n: I18N;
 
-	constructor(http: HttpClient, dialogService: DialogService, settingsManager: SettingsManager, i18n: I18N, router: Router) {
-		this._http = http;
+	constructor(service: QuickRecipeService, dialogService: DialogService, settingsManager: SettingsManager, i18n: I18N, router: Router) {
+		this._service = service;
 		this._i18n = i18n;
 		this._router = router;
 		this._dialogService = dialogService;
@@ -35,8 +35,7 @@ export class QuickRecipeHeader {
 	}
 
 	reportRecipe(): void {
-		this._http
-            .put("http://eleeapi.azurewebsites.net/api/review/flag/" + this.recipeId)
+		this._service.report(this.recipeId)
             .then(response => {
                 Materialize.toast(this._i18n.tr("quickRecipe.recipeFlagged"), 3000)
             });
