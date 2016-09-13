a81e7232c3398e04131f1ed6876fc7163160de7a
Marc-André Roy <marc596@gmail.com>
2016-09-12 20:39:21 -0400

    Add missing closing bracket in administration.ts and bring back autoinject in advanced search component...

--- Source/app/shared/components/advanced-search/advanced-search.ts
+++ Source/app/shared/components/advanced-search/advanced-search.ts
@@ -1,8 +1,8 @@
-import {inject, bindable} from "aurelia-framework";
+import {autoinject, bindable} from "aurelia-framework";
 import {EventAggregator} from "aurelia-event-aggregator";
 import {I18N, BaseI18N} from "aurelia-i18n";
 
-@inject(I18N, Element, EventAggregator)
+@autoinject
 export class AdvancedSearch extends BaseI18N {
   @bindable time: string = null;
   @bindable ingredients: string[] = [];
