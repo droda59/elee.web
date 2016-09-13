b92a46c4ba6c5a00dad0111cd5f08199a916678e
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-09-13 11:03:31 -0400

    Added support for autowidth select

--- Source/app/shared/components/auto-width-input.ts
+++ Source/app/shared/components/auto-width-input.ts
@@ -1,26 +1,18 @@
-import {customAttribute, inject} from "aurelia-framework";
+import {bindable, customAttribute, inject} from "aurelia-framework";
 
 @customAttribute("autowidth")
 @inject(Element)
 export class AutoWidthInput {
-    private _keypressCallback: (e: KeyboardEvent) => void;
+    @bindable() calculateWidth = "";
+
     private _element: Element;
 
     constructor(element: Element) {
         this._element = element;
-        this._keypressCallback = this._keypressInput;
     }
 
-    attached() {
-        this._element.addEventListener("keyup", this._keypressCallback);
-        this._element.style.width = ((this._element.value.length + 1) * 9) + "px";
-    }
-
-    detached() {
-        this._element.removeEventListener("keyup", this._keypressCallback);
-    }
-
-    private _keypressInput(e) {
-        e.target.style.width = ((e.target.value.length + 1) * 9) + "px";
+    calculateWidthChanged(newValue) {
+        var width = (((newValue || "").length + 1) * 9) + "px";
+        this._element.style.width = width;
     }
 }
