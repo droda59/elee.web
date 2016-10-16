81ee993b5847586063ae338068e5b9ba4de904bd
Marc-André Roy <marc596@gmail.com>
2016-10-16 16:12:14 -0400

    Some syntax sugar. Use typescript auto-assignment.

--- Source/app/shared/components/advanced-search/advanced-search.ts
+++ Source/app/shared/components/advanced-search/advanced-search.ts
@@ -7,13 +7,9 @@ export class AdvancedSearch extends BaseI18N {
   @bindable time: string|null;
   @bindable ingredients: string[] = [];
 
-  private _i18n: I18N;
-  private _element: Element;
-
-  constructor(i18n: I18N, element: Element, ea: EventAggregator) {
+  constructor(private i18n: I18N, 
+              private element: Element, 
+              ea: EventAggregator) {
     super(i18n, element, ea);
-
-    this._i18n = i18n;
-    this._element = element;
   }
 }
