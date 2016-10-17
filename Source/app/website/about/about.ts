2c28afec1d0a4cf85f4f592537c391d3b09d617e
Marc-André Roy <marc596@gmail.com>
2016-10-17 17:40:23 -0400

    Attempt at making the about page a little better. Not completed tought, just leaving it here...

--- Source/app/website/about/about.ts
+++ Source/app/website/about/about.ts
@@ -1,22 +1,19 @@
-import {Router} from "aurelia-router";
-import {autoinject, inject} from "aurelia-framework";
-import {I18N, BaseI18N} from "aurelia-i18n";
-import {EventAggregator} from "aurelia-event-aggregator";
+import { Router } from "aurelia-router";
+import { autoinject, inject } from "aurelia-framework";
+import { I18N, BaseI18N } from "aurelia-i18n";
+import { EventAggregator } from "aurelia-event-aggregator";
 
 // @autoinject
 @inject(Router, I18N, Element, EventAggregator)
 export class About extends BaseI18N {
-  private _router: Router;
-  private _element: Element;
+	constructor(private router: Router,
+							private i18n: I18N,
+							private element: Element,
+							ea: EventAggregator) {
+		super(i18n, element, ea);
+	}
 
-  constructor(router: Router, i18n: I18N, element: Element, ea: EventAggregator) {
-    super(i18n, element, ea);
-
-    this._router = router;
-    this._element = element;
-  }
-
-  activate(route, routeConfig) {
-    // Nothin' yet
-  }
+	activate(route, routeConfig) {
+		// Nothin' yet
+	}
 }
