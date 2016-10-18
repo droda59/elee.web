327a785fd887bf41eb136d88b1754cd3a08007c6
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-10-18 11:47:22 -0400

    Fix on dragula dependency

--- Source/build/tasks/export.js
+++ Source/build/tasks/export.js
@@ -36,6 +36,7 @@ gulp.task("export:copy", function () {
         "jspm_packages/system-csp-production.js",
         "jspm_packages/github/systemjs/plugin-text@**.js",
         "jspm_packages/github/systemjs/plugin-text@**/text.js",
+        "jspm_packages/npm/aurelia-dragula@**/dragula-and-drop.js",
         "jspm_packages/npm/jquery@**/dist/jquery.min.js",
         "jspm_packages/npm/materialize-css@**/dist/js/materialize.min.js",
         "jspm_packages/npm/moment@**/moment.js",
