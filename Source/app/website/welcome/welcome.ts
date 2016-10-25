2e3194cc5fa075e919d34ba4fc703d7081cdc303
Marc-André Roy <marc596@gmail.com>
2016-10-24 22:12:48 -0400

    Send advanced search data to API.

--- Source/app/website/welcome/welcome.ts
+++ Source/app/website/welcome/welcome.ts
@@ -1,27 +1,34 @@
-import {Router} from "aurelia-router";
-import {autoinject} from "aurelia-framework";
-import {QuickRecipeService} from "app/quick-recipe/shared/quick-recipe-service";
-import {QuickRecipeSearchResult} from "app/quick-recipe/shared/models/quick-recipe-search-result";
+import { Router } from "aurelia-router";
+import { autoinject } from "aurelia-framework";
+import { QuickRecipeService } from "app/quick-recipe/shared/quick-recipe-service";
+import { QuickRecipeSearchResult } from "app/quick-recipe/shared/models/quick-recipe-search-result";
 
 @autoinject()
 export class Welcome {
 	results: Array<QuickRecipeSearchResult> = undefined;
-	ingredients: Array<Object> = [];
+	ingredients: Array<SearchIngredient> = [];
 	searchTerms: string;
 	maximumTime: number = 0;
 
 	constructor(private _router: Router,
-				private _service: QuickRecipeService) {
+		private _service: QuickRecipeService) {
 	}
 
 	searchRecipes(): void {
 		if (this.searchTerms && this.searchTerms.length >= 3) {
 			let searchContainer = $("#search-container")[0];
-			let ingredientsConcat: string = "";
 			console.log(`${JSON.stringify(this.ingredients)}`);
 
-			for(let i = 0; i < this.ingredients.length; i++) {
-				ingredientsConcat += this.ingredients[i].tag + (i < this.ingredients.length) ? ",":"";
+			let ingredientsConcat: string = "";
+			if (this.ingredients.length > 0) {
+				for (let i = 0; i < this.ingredients.length; i++) {
+					ingredientsConcat += this.ingredients[i].tag;
+
+					if(i < this.ingredients.length - 1) {
+						ingredientsConcat += ",";
+					}
+					console.log(`Include Ingredient : ${this.ingredients[i].tag}`);
+				}
 			}
 
 			this._service.findRecipesAdvanced(this.searchTerms, ingredientsConcat, this.maximumTime)
@@ -37,4 +44,8 @@ export class Welcome {
 	loadRecipe(id: string): void {
 		this._router.navigateToRoute("quick-recipe", { "id": id }, undefined);
 	}
+}
+
+interface SearchIngredient {
+	tag: string;
 }
--- Source/config.js
+++ Source/config.js
@@ -6,6 +6,7 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
+
   packages: {
     "materialize-css": {
       "main": "dist/js/materialize.min.js",
@@ -14,6 +15,7 @@ System.config({
       }
     }
   },
+
   map: {
     "animate.css": "npm:animate.css@3.5.2",
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.1",
