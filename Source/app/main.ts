07d9a570b392db4d72b0112e0ff412801b41707b
Maxime Gaudreault Proulx <swcommander@gmail.com>
2016-11-02 22:04:54 -0400

    Configuration of admin routes based on current environment

--- Source/app/main.ts
+++ Source/app/main.ts
@@ -1,54 +1,65 @@
-import {inject} from "aurelia-framework";
+import {autoinject} from "aurelia-framework";
 import {Router, RouterConfiguration} from "aurelia-router";
+import {Configure} from "aurelia-configuration";
 import {I18N} from "aurelia-i18n";
 
-@inject(I18N)
+@autoinject()
 export class Main {
     router: Router;
-    private _i18n: I18N;
 
-    constructor(i18n: I18N) {
+    private _i18n: I18N;
+    private _configuration: Configure;
+
+    constructor(i18n: I18N, configuration: Configure) {
         this._i18n = i18n;
+        this._configuration = configuration;
     }
 
     configureRouter(config: RouterConfiguration, router: Router) {
-        config.title = "E133";
-        config.map([
-        {
-            route: ["", "home"],
-            moduleId: "app/website/home",
-            nav: false
-        },
-        {
-            route: "typeform",
-            name: "typeform",
-            moduleId: "app/shared/components/typeform",
-            nav: true,
-            title: this._i18n.tr("comments.pageTitle")
-        },
-        // {
-        //     route: "administration/recipes",
-        //     name: "administration",
-        //     moduleId: "app/quick-recipe/administration/index",
-        //     title: this._i18n.tr("administration.pageTitle")
-        // },
-        // {
-        //     route: "administration/backgrounds",
-        //     name: "backgrounds",
-        //     moduleId: "app/quick-recipe/backgrounds/index",
-        //     title: this._i18n.tr("backgrounds.pageTitle")
-        // },
-        {
-            route: "recipe/:id",
-            name: "quick-recipe",
-            moduleId: "app/quick-recipe/follow-recipe/index"
-        // },
-        // {
-        //     route: "recipe/:id/edit",
-        //     name: "edit",
-        //     moduleId: "app/quick-recipe/edit-recipe/index"
+        let routes = [
+            {
+                route: ["", "home"],
+                moduleId: "app/website/home",
+                nav: false
+            },
+            {
+                route: "typeform",
+                name: "typeform",
+                moduleId: "app/shared/components/typeform",
+                nav: true,
+                title: this._i18n.tr("comments.pageTitle")
+            },
+            {
+                route: "recipe/:id",
+                name: "quick-recipe",
+                moduleId: "app/quick-recipe/follow-recipe/index"
+            }
+        ];
+
+        if (this._configuration.is("development")) {
+            routes.push({
+                route: "administration/recipes",
+                name: "administration",
+                moduleId: "app/quick-recipe/administration/index",
+                title: this._i18n.tr("administration.pageTitle")
+            });
+
+            routes.push({
+                route: "administration/backgrounds",
+                name: "backgrounds",
+                moduleId: "app/quick-recipe/backgrounds/index",
+                title: this._i18n.tr("backgrounds.pageTitle")
+            });
+
+            routes.push({
+                route: "recipe/:id/edit",
+                name: "edit",
+                moduleId: "app/quick-recipe/edit-recipe/index"
+            });
         }
-        ]);
+
+        config.title = "E133";
+        config.map(routes);
 
         this.router = router;
     }
