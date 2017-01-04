484d753556300785fea57f3b6d742effaa245edf
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-01-03 21:54:51 -0500

    Some small adjustments on the about page

--- Source/app/website/about/about.ts
+++ Source/app/website/about/about.ts
@@ -1,19 +1,2 @@
-import { Router } from "aurelia-router";
-import { autoinject, inject } from "aurelia-framework";
-import { I18N, BaseI18N } from "aurelia-i18n";
-import { EventAggregator } from "aurelia-event-aggregator";
-
-// @autoinject
-@inject(Router, I18N, Element, EventAggregator)
-export class About extends BaseI18N {
-	constructor(private router: Router,
-							private i18n: I18N,
-							private element: Element,
-							ea: EventAggregator) {
-		super(i18n, element, ea);
-	}
-
-	activate(route, routeConfig) {
-		// Nothin' yet
-	}
+export class About {
 }
