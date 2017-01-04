ac73ec7439b64f1801c026cda0f40cd3d853424d
Maxime Gaudreault Proulx <swcommander@gmail.com>
2017-01-03 22:27:19 -0500

    Some content in the contact form; Creation of a contact service

--- Source/app/website/contact/contact.ts
+++ Source/app/website/contact/contact.ts
@@ -1,2 +1,22 @@
+import { autoinject } from "aurelia-framework";
+import { ContactService } from "app/quick-recipe/shared/contact-service";
+import { ContactForm } from "app/quick-recipe/shared/models/contact-form";
+
+@autoinject()
 export class ContactPage {
+    private _contactService: ContactService;
+
+    email: string;
+    name: string;
+    message: string;
+
+    constructor(contactService: ContactService) {
+        this._contactService = contactService;
+    }
+
+    send() {
+        let contactForm: ContactForm = { email: this.email, name: this.name, message: this.message};
+
+        // this._contactService.send(contactForm);
+    }
 }
