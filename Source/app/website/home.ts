efadedd2416c44b3f38a9471709ba90e5990faf1
Marc-André Roy <marc596@gmail.com>
2017-01-06 21:46:54 -0500

    Remote property get for IsHomepage since it reevaluate it like crazy for nothing.

--- Source/app/website/home.ts
+++ Source/app/website/home.ts
@@ -1,49 +1,45 @@
-import {inject} from "aurelia-framework";
-import {Router, RouterConfiguration} from "aurelia-router";
-import {I18N} from "aurelia-i18n";
+import { autoinject } from "aurelia-framework";
+import { Router, RouterConfiguration } from "aurelia-router";
+import { I18N } from "aurelia-i18n";
 
-@inject(I18N)
+@autoinject
 export class Home {
-  router: Router;
+	router: Router;
 	isHomepage: boolean;
 
-  constructor(private _i18n: I18N) { }
+	constructor(private _i18n: I18N) { }
 
-  configureRouter(config: RouterConfiguration, router: Router) {
-    config.map([
-      {
-        route: ["", "welcome"],
-        name: "welcome",
-        moduleId: "app/website/welcome/welcome",
-        nav: true,
-        title: this._i18n.tr("home.pageTitle")
-      },
-      {
-        route: "about",
-        name: "about",
-        moduleId: "app/website/about/about",
-        nav: true,
-        href: "#/home/about/",
-        title: this._i18n.tr("about.pageTitle")
-      },
-      {
-        route: "contact",
-        name: "contact",
-        moduleId: "app/website/contact/contact",
-        nav: true,
-        href: "#/home/contact/",
-        title: this._i18n.tr("contact.pageTitle")
-      }
-    ]);
+	configureRouter(config: RouterConfiguration, router: Router) {
+		config.map([
+			{
+				route: ["", "welcome"],
+				name: "welcome",
+				moduleId: "app/website/welcome/welcome",
+				nav: true,
+				title: this._i18n.tr("home.pageTitle")
+			},
+			{
+				route: "about",
+				name: "about",
+				moduleId: "app/website/about/about",
+				nav: true,
+				href: "#/home/about/",
+				title: this._i18n.tr("about.pageTitle")
+			},
+			{
+				route: "contact",
+				name: "contact",
+				moduleId: "app/website/contact/contact",
+				nav: true,
+				href: "#/home/contact/",
+				title: this._i18n.tr("contact.pageTitle")
+			}
+		]);
 
-    this.router = router;
-  }
-
-	get isHomePageActive(): boolean {
-		return this.getCurrentPageName === "welcome";
+		this.router = router;
 	}
 
-	get getCurrentPageName(): string {
+	getCurrentPageName(): string {
 		return this.router.currentInstruction.config.name;
 	}
 }
