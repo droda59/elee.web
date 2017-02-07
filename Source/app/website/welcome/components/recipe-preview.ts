2c2eec126be1fe4e5e5f676f79aa13160e70c5b9
Marc-André Roy <marc596@gmail.com>
2017-02-06 21:28:29 -0500

    Component base.

--- Source/app/website/welcome/components/recipe-preview.ts
+++
@@ -1,9 +0,0 @@
-import { autoinject, customElement } from 'aurelia-framework';
-
-@customElement('recipe-preview')
-@autoinject
-export class RecipePreview {
-	constructor(private element: Element) {
-
-	}
-}
